5bc6a5030a47c00309eee651ccda454da1e20e5e
tchoforwilson <tchoforwilson@gmail.com>
2024-08-24 11:05:04 +0100

    Defined Product and customer model fields

--- store/models.py
+++ store/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_updated = models.DateTimeField(auto_now=True)
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True, max_length=30, null=False)
+    phone = models.CharField(max_length=13)
+    birth_date = models.DateField(null=True)
