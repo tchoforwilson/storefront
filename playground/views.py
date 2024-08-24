1bc4d43cc62b2495d845f0d788d6ce43b13615d6
tchoforwilson <tchoforwilson@gmail.com>
2024-08-24 09:46:28 +0100

    Template demo

--- playground/views.py
+++ playground/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse('Hello, world!')
+    return render(request, 'hello.html', {'name':"John Doe"})
