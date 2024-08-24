0f16de64581e34771e3b434b32d101191950c700
tchoforwilson <tchoforwilson@gmail.com>
2024-08-24 10:03:13 +0100

    Debugging application

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def calculate():
+    x = 1
+    y = 2
+    return x + y
+
 def say_hello(request):
+    x = calculate()
     return render(request, 'hello.html', {'name':"John Doe"})
