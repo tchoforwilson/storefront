16c525bfd879ea2f1adb4aa5fe51473d04170a0a
tchoforwilson <tchoforwilson@gmail.com>
2024-08-24 11:16:07 +0100

    Define order model fields and define choices for customer and order

--- store/models.py
+++ store/models.py
@@ -8,8 +8,34 @@ class Product(models.Model):
     last_updated = models.DateTimeField(auto_now=True)
 
 class Customer(models.Model):
+    
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+    
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True, max_length=30, null=False)
     phone = models.CharField(max_length=13)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'p',
+    PAYMENT_STATUS_COMPLETED = 'C',
+    PAYMENT_STATUS_FAILED = 'F',
+    
+    PAYMENT_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETED, 'Completed'),
+        (PAYMENT_STATUS_FAILED, 'Failed'),
+    ]
+    
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_CHOICES, default=PAYMENT_STATUS_PENDING)
