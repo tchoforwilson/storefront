1aee9974fce94c38ee7af654f138d5c06602871d
tchoforwilson <tchoforwilson@gmail.com>
2024-08-30 20:33:43 +0100

    Executing raw queries

--- playground/views.py
+++ playground/views.py
@@ -1,21 +1,12 @@
 from django.shortcuts import render
-from django.db import transaction
+from django.db import connection
 from store.models import Order, OrderItem, Product, Customer, Collection
 
 def say_hello(request):
-    # ...
     
-    with transaction.atomic():
-        order = Order()
-        order.customer_id = 1
-        order.save()
-        
-        item = OrderItem()
-        item.order = order
-        item.product_id = -1
-        item.quantity = 1
-        item.unit_price = 10
-        
-        item.save()
+    with connection.cursor() as cursor:
     
-    return render(request, 'hello.html', {'name':'John Doe'})
+        cursor.callproc('get_customers', [1, 2, 3, 'a'])
+    
+    
+    return render(request, 'hello.html', {'name':'John Doe', 'result':list(queryset)})
