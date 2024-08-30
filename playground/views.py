c518017c4605df626b254e7f3baaa5b1aa711938
tchoforwilson <tchoforwilson@gmail.com>
2024-08-30 20:01:27 +0100

    Implemented a custom tag manager

--- playground/views.py
+++ playground/views.py
@@ -4,11 +4,5 @@ from store.models import Product
 from tags.models import TaggedItem
 
 def say_hello(request):
-    content_type = ContentType.objects.get_for_models(Product)
-    queryset = TaggedItem.objects\
-        .select_related('tag')\
-        .filter(
-            content_type=content_type, 
-            object_id=1
-        )
+    queryset = TaggedItem.objects.ge_tags_for(Product, 1)
     return render(request, 'hello.html', {'name':'John Doe', 'tags':list(queryset)})
