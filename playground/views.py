097e43087b45d9110808faa71586567ac187a0fb
tchoforwilson <tchoforwilson@gmail.com>
2024-08-30 20:20:30 +0100

    Updating objects

--- playground/views.py
+++ playground/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from store.models import Order, OrderItem, Product, Customer, Collection
 
 def say_hello(request):
-    collection = Collection()
-    collection.title = "Video Games"
-    collection.featured_product = Product(pk=1)
+    collection = Collection.objects.get(pk=11)
+    collection.featured_product = None
     collection.save()
     
     return render(request, 'hello.html', {'name':'John Doe'})
