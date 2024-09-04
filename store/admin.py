22f9880528245ad32ba4a2f411b5de5b42d6e914
tchoforwilson <tchoforwilson@gmail.com>
2024-09-04 20:58:37 +0100

    Implementing filtering lists

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,29 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
 from . import models
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+    
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low'),
+            ('>10', 'High'),
+        ]
+    
+    def queryset(self, request, queryset: QuerySet):
+        if(self.value() == '<10'):
+            return queryset.filter(inventory__lt=10)
+        if(self.value() == '>10'):
+            return queryset.filter(inventory__gt=10)
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price','inventory_status', 'collection']
     list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 10
     
     @admin.display(ordering='inventory')
